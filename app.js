a4351d20707d5864af9af138beac03bc67bd8f73
LuisGlzg <luisyael1112@gmail.com>
2025-03-01 02:39:24 -0600

    actualiza el app.js

--- app.js
+++ app.js
@@ -2,14 +2,23 @@
 
 // Se crean las variables a utilizar
 let nombres = [];
+let intento = 0;
 
 function agregarAmigo(){
-    let nombrehtml = document.querySelector(input);
+    let nombrehtml = document.getElementById('amigo').value;
     nombres.push(nombrehtml);
 
+    if (intento == 0) nombres.pop();
+    if (nombres[intento] == '') nombres.pop();
     console.log(nombres);
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = nombres;
+    intento++;
+    limpiarCaja();
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
 }
 
 agregarAmigo();
