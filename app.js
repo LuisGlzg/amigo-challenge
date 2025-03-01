34f493d8549e6af329571892764dfbb8c68fc210
LuisGlzg <luisyael1112@gmail.com>
2025-03-01 02:10:42 -0600

    actuaiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Se crean las variables a utilizar
+let nombres = [];
+
+function agregarAmigo(){
+    let nombrehtml = document.querySelector(input);
+    nombres.push(nombrehtml);
+
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = nombres;
+}
+
+agregarAmigo();
